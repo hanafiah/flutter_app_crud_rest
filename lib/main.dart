db36f0bc6c5db7038dc8f2d6aeb7e7a8aaa4b7b6
ibnuyahya <ibnuyahya@gmail.com>
2020-02-19 01:18:38 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_crud_rest/add_profile.dart';
+
+import 'home.dart';
+
+void main() => runApp(App());
+
+GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+        accentColor: Colors.purpleAccent,
+      ),
+      home: Scaffold(
+        key: _scaffoldState,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              _scaffoldState.currentContext,
+              MaterialPageRoute(builder: (BuildContext context) {
+                return FormAddScreen();
+              }),
+            );
+            // Add your onPressed code here!
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.purple,
+        ),
+        appBar: AppBar(
+          title: Text(
+            "Flutter CRUD API",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+
+        ),
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
